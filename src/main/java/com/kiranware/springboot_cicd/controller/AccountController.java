141cdaa902b679d75407b5eb191ed7fba9572e89
kiranware <kiranware13@gmail.com>
2025-12-22 15:50:06 +0530

    codes changes in controller to print message

--- src/main/java/com/kiranware/springboot_cicd/controller/AccountController.java
+++ src/main/java/com/kiranware/springboot_cicd/controller/AccountController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     @GetMapping
     public String getAccountDetails() {
-        return "Account belong to KIRAN WARE.";
+        return "Account belong to KIRAN WARE and POOJA Andhale.";
     }
 }
